450c248aa4335d15199807dcd53cbf907c5ce794
AMandac252 <113184877+AMandac252@users.noreply.github.com>
2023-02-25 20:02:40 -0500

    beep boop

--- create-task/js/cats.js
+++ create-task/js/cats.js
@@ -1,5 +1,5 @@
 async function fetchcat() {
-  let nekos = [];
+  let cats = [];
   for (let i = 1; i < 67; i++) {
     let url = `https://api.neko-atsume.emshea.com/cats/${i}`;
     const response = await fetch(url);
@@ -9,25 +9,25 @@ async function fetchcat() {
       throw error(response);
     } else {
       const cat = await response.json();
-      nekos.push(cat);
+      cats.push(cat);
     }
   }
-  showcats(nekos);
+  showcats(cats);
 }
 
-function showcats(nekos) {
-  cat.forEach((neko) => {
-    document.getElementById("catdisplay").insertAdjacentHTML(
+function showcats(cats) {
+  document.getElementById("display").innerHTML = "";
+  cats.forEach((cat) => {
+    document.getElementsById("display").insertAdjacentHTML(
       "beforeend",
 
       `<div data-aos="fade-up" class="display-card2">
-  <p> cat id number ${neko.CatID}</p>
-  <img class="display-sprite" src="${neko.CatImage}" />
-  <h4 class="display-name">${neko.CatName}</h4>
-  <h5 class="display-type"> Cat type: ${neko.CatType}</h5>`
+  <p> cat id number ${cat.CatID}</p>
+  <img class="display-sprite" src="${cat.CatImage}" />
+  <h4 class="display-name">${cat.CatName}</h4>
+  <h5 class="display-type"> Cat type: ${cat.CatType}</h5>`
     );
   });
 }
-fetchcat();
 
 export { fetchcat };
--- create-task/js/main.js
+++ create-task/js/main.js
@@ -9,4 +9,41 @@ AOS.init();
 const DOMSelectors = {
   display: document.querySelector(".display"),
   cats: document.querySelector("cats"),
+  // mainbtn: document.querySelector(".home-btn"),
+  // history: document.querySelector(".history-btn"),
 };
+
+// DOMSelectors.mainbtn.addEventListener("click", function () {
+//   catintro();
+// });
+
+// DOMSelectors.history.addEventListener("click", function () {
+//   cathistory();
+// });
+
+// function catintro() {
+//   DOMSelectors.display.innerHTML = "";
+
+//   DOMSelectors.maininfo.insertAdjacentHTML(
+//     "beforeend",
+
+//     `<div data-aos="fade-up" class="display-card">
+//       <h3 class="info">Welcome to the Map </h3>
+//       <p>These are all the current locations. </p>
+//     </div>`
+//   );
+//   fetchcat();
+// }
+
+// function cathistory() {
+//   DOMSelectors.display.innerHTML = "";
+
+//   DOMSelectors.maininfo.insertAdjacentHTML(
+//     "beforeend",
+
+//     `<div data-aos="fade-up" class="display-card">
+//       <h3 class="info">Welcome to the History </h3>
+//     </div>`
+//   );
+//   fetchhistory();
+// }
